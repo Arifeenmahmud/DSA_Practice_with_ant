a3b41eea1e2c8c7eaf3e891af14af97582c1dbe1
arifeenmahmud <arifeen.mahmud@gmail.com>
2023-07-15 21:28:45 +0600

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // Counter for elements not equal to val
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example usage:
+        int[] nums1 = {3, 2, 2, 3};
+        int val1 = 3;
+        int k1 = solution.removeElement(nums1, val1);
+        System.out.println("Output: " + k1 + ", nums1 = " + Arrays.toString(nums1));
+
+        int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val2 = 2;
+        int k2 = solution.removeElement(nums2, val2);
+        System.out.println("Output: " + k2 + ", nums2 = " + Arrays.toString(nums2));
+    }
+}
